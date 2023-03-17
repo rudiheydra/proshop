d0e9e921c7dbc19053813245e5f58755ff313a13
Rudi Heydra <rudi.heydra@gmail.com>
2023-03-17 14:02:25 +1100

    Implementing server

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 // import path from 'path'
 import express from "express";
+import products from "./data/products.js";
 // import dotenv from 'dotenv'
 // import colors from 'colors'
 // import morgan from 'morgan'
@@ -60,6 +61,13 @@ const app = express();
 // )
 app.get("/", (req, res) => {
   res.send("API is running....");
+});
+app.get("/api/products", (req, res) => {
+  res.json(products);
+});
+app.get("/api/products/:id", (req, res) => {
+  const product = products.find((p) => p._id === req.params.id);
+  res.json(products);
 });
 
 app.listen(5000, console.log("Server listening on port 5000"));
