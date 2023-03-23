fea67e4a23ae4de37f2cacb58a94b41030b47238
Rudi Heydra <rudi.heydra@gmail.com>
2023-03-23 21:55:31 +1100

    adding redux store and custom error handeling

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,15 @@
-// import path from 'path'
+import path from "path";
 import express from "express";
-import products from "./data/products.js";
 import dotenv from "dotenv";
 import colors from "colors";
-// import morgan from 'morgan'
-// import { notFound, errorHandler } from './middleware/errorMiddleware.js'
+import morgan from "morgan";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import connectDB from "./config/db.js";
 
-// import productRoutes from './routes/productRoutes.js'
-// import userRoutes from './routes/userRoutes.js'
-// import orderRoutes from './routes/orderRoutes.js'
-// import uploadRoutes from './routes/uploadRoutes.js'
+import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
+import uploadRoutes from "./routes/uploadRoutes.js";
 
 dotenv.config();
 
@@ -18,38 +17,38 @@ connectDB();
 
 const app = express();
 
-// if (process.env.NODE_ENV === 'development') {
-//   app.use(morgan('dev'))
-// }
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 
-// app.use(express.json())
+app.use(express.json());
 
-// app.use('/api/products', productRoutes)
-// app.use('/api/users', userRoutes)
-// app.use('/api/orders', orderRoutes)
-// app.use('/api/upload', uploadRoutes)
+app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/orders", orderRoutes);
+app.use("/api/upload", uploadRoutes);
 
-// app.get('/api/config/paypal', (req, res) =>
-//   res.send(process.env.PAYPAL_CLIENT_ID)
-// )
+app.get("/api/config/paypal", (req, res) =>
+  res.send(process.env.PAYPAL_CLIENT_ID)
+);
 
-// const __dirname = path.resolve()
-// app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+const __dirname = path.resolve();
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.join(__dirname, '/frontend/build')))
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
 
-//   app.get('*', (req, res) =>
-//     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-//   )
-// } else {
-//   app.get('/', (req, res) => {
-//     res.send('API is running....')
-//   })
-// }
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running....");
+  });
+}
 
-// app.use(notFound)
-// app.use(errorHandler)
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
@@ -59,13 +58,3 @@ app.listen(
     `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
   )
 );
-app.get("/", (req, res) => {
-  res.send("API is running....");
-});
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,47 +1,48 @@
 import React, { useEffect, useState } from "react";
 
-import axios from "axios";
+// import axios from "axios";
 // import { Link } from 'react-router-dom'
-// import { useDispatch, useSelector } from 'react-redux'
+import { useDispatch, useSelector } from "react-redux";
 import { Row, Col } from "react-bootstrap";
 import Product from "../components/Product";
-// import Message from '../components/Message'
-// import Loader from '../components/Loader'
+import Message from "../components/Message";
+import Loader from "../components/Loader";
 // import Paginate from '../components/Paginate'
 // import ProductCarousel from '../components/ProductCarousel'
 // import Meta from '../components/Meta'
-// import { listProducts } from '../actions/productActions'
+import { listProducts } from "../actions/productActions";
 
 const HomeScreen = () => {
   // const keyword = match.params.keyword;
 
   // const pageNumber = match.params.pageNumber || 1;
 
-  // const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-  // const productList = useSelector((state) => state.productList);
-  // const { loading, error, products, page, pages } = productList;
-  const [products, setProducts] = useState([]);
+  const productList = useSelector((state) => state.productList);
+  const { loading, error, products, page, pages } = productList;
+  // const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    const fetchProducts = async () => {
-      const { data } = await axios.get("/api/products");
-
-      setProducts(data);
-    };
-    fetchProducts();
-  }, []);
+    dispatch(listProducts());
+  }, [dispatch]);
 
   return (
     <>
       <h1>Latest Products</h1>
-      <Row>
-        {products.map((product) => (
-          <Col key={product.name} sm={12} md={6} ld={4} xl={3}>
-            <Product product={product} />
-          </Col>
-        ))}
-      </Row>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant="danger">{error}</Message>
+      ) : (
+        <Row>
+          {products.map((product) => (
+            <Col key={product.name} sm={12} md={6} ld={4} xl={3}>
+              <Product product={product} />
+            </Col>
+          ))}
+        </Row>
+      )}
     </>
   );
 };
